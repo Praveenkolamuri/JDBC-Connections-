89b5f94214eb0662e8285bb10f04f93a0f891663
Praveen Kolamuri <152489796+Praveenkolamuri@users.noreply.github.com>
2024-07-17 15:11:55 +0530

    Update deleteservlet.java

--- application-1/src/main/java/servlet/deleteservlet.java
+++ application-1/src/main/java/servlet/deleteservlet.java
@@ -1,20 +1,41 @@
 package servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class deleteservlet  extends HttpServlet{
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		try {
-			
-		} catch (Exception e) {
-			
-		}
-	}
-
+@WebServlet("/delete")
+public class deleteservlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/demo-1", "root", "Praveen#1221");
+            String sql = "DELETE FROM employee WHERE rollno = ?";
+            PreparedStatement pmst = connect.prepareStatement(sql);
+            String rollno = req.getParameter("rollno");
+            
+            pmst.setString(1, rollno);
+            
+            int i = pmst.executeUpdate();
+            PrintWriter pw = resp.getWriter();
+            
+            if (i > 0) {
+                pw.println("Successfully deleted");
+            } else {
+                pw.println("Error: No record found with the provided rollno");
+            }
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
